c81cef95bcbef490c9ea5a44cd76cb80d0105002
valentinaspopov <valentinaspopov@gmail.com>
2024-11-20 21:35:19 +0200

    Papildyta funkcija "read_from_website" Prideta funkcija "save_from_website"

--- valentinas_p_mod1_atsiskaitymas/main.py
+++ valentinas_p_mod1_atsiskaitymas/main.py
@@ -3,7 +3,8 @@ from web_crawling import WebCrawling
 
 def main():
     crawling = WebCrawling()
-    crawling.read_from_website("eurovaistine.lt")
+    crawling.read_from_website("eurovaistine.lt", 5)
+    crawling.save_file()
 
 if __name__ == '__main__':
     main()
--- valentinas_p_mod1_atsiskaitymas/web_crawling.py
+++ valentinas_p_mod1_atsiskaitymas/web_crawling.py
@@ -1,50 +1,80 @@
+import csv
+import json
+
 from requests import get
 from lxml.etree import HTML
+from time import time
+
 
 class WebCrawling:
     def __init__(self):
-        pass
-    def read_from_website(self, selected_site):
+        self.vaistaiList = []
+        self.straipsniaiList = []
 
+    def read_from_website(self, selected_site, timeout_duration):
+        start_time = time()
 
-        if selected_site == "eurovaistine.lt":
+        if selected_site == "camelia.lt":
             url = "https://camelia.lt/c/prekiu-medis/vitaminai-maisto-papildai-mineralai/groziui/plaukams-1-1522"
         elif selected_site == "lrytas.lt":
             url = "https://lrytas.lt"
         else:
-            raise ValueError("svetaine nera sarase")
+            raise ValueError("The website is not in the list.")
 
         response = get(url)
         text = response.text
         tree = HTML(text)
-        pavadinimasList = []
-        paveikslaiList = []
 
-        match selected_site:
-            case "eurovaistine.lt":
-                elements = tree.xpath("//div[contains(@class, 'product-card')]")
-                for element in elements:
-                    try:
-                        pavadinimas_text = element.xpath(".//div[contains(@class,'product-name')]/text()")
-                        if pavadinimas_text:
-                            pavadinimasList.append(pavadinimas_text[0].strip())
+        # Jeigu pasirinktas "camelia.lt"
+        if selected_site == "camelia.lt":
+            elements = tree.xpath("//div[contains(@class, 'product-card')]")
+            for element in elements:
+                if time() - start_time > timeout_duration:
+                    break
+                try:
+                    pavadinimas_text = element.xpath(".//div[contains(@class,'product-name')]/text()")
+                    paveikslai_text = element.xpath(".//div/img[contains(@class,'product-image')]/@src")
 
-                        paveikslai_text = element.xpath(".//div/img[contains(@class,'product-image')]/@src")
-                        if paveikslai_text:
-                            paveikslaiList.append(paveikslai_text[0].strip())
-                    except IndexError:
-                        print("Elementai nerasti")
+                    if pavadinimas_text and paveikslai_text:
+                        self.vaistaiList.append((
+                            pavadinimas_text[0].strip(),
+                            paveikslai_text[0].strip()
+                        ))
+                except IndexError:
+                    print("Nerasta elementu")
+                return self.vaistaiList
 
+        #Jeigu pasirinktas "lrytas.lt"
+        if selected_site == "lrytas.lt":
+            return self.straipsniaiList
 
-        #print("Produktu pavadinimai: ")
-        #for name in pavadinimasList:
-           #print(name)
+    def save_file(self, selected_site, filename = " ", format = " "):
 
-        #print("\n Nuotraukos:")
-        #for img_url in paveikslaiList:
-            #print(img_url)
+        currentList = []
 
+        if selected_site == "camelia.lt":
+            currentList = self.vaistaiList
+        elif selected_site == "lrytas.lt":
+            currentList = self.straipsniaiList
+        else:
+            raise ValueError("Nera tokios svetainės sąraše")
 
+        #issaugojama i csv faila
+        if format == "list":
+            return currentList
+        elif format == "csv":
+            with open(filename, "w", newline='', ) as cswFile:
+                writer = csv.writer(cswFile)
+                writer.writerows(currentList)  # Writing data
+            print(f"Issaugojama i  {filename} . {format}")
 
-    def save_from_website(self, format):
-        pass
+        elif format == "csv":
+            with open(filename, "w", newline='', ) as cswFile:
+                writer = csv.writer(cswFile)
+                writer.writerows(currentList)  # Writing data
+            print(f"Issaugojama i  {filename} . {format}")
+        elif format == "json":
+            with open(filename, "w", newline='', ) as jsonFile:
+                json.dump(currentList, jsonFile)
+        else:
+            raise ValueError("Netinkamas formatas, pasirinkite list, csv arba json")
